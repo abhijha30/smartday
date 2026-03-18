4c2f8e5f27ce5d52f0eed957c2edeb9433f88034
Avinash Kumar Jha <149184004+abhijha30@users.noreply.github.com>
2026-03-18 12:54:56 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const SUPABASE_URL = "YOUR_URL";
-const SUPABASE_KEY = "YOUR_ANON_KEY";
+const SUPABASE_URL = "https://umryashhwokftkxvkozq.supabase.co";
+const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InVtcnlhc2hod29rZnRreHZrb3pxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM4MDkyNTgsImV4cCI6MjA4OTM4NTI1OH0.wUepcoz1kbyoOgILhRD1iLZDbRpbT-igTXWJu0NSmwY";
 
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
